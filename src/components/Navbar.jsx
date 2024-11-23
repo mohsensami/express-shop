631d55d6ea9b5006642150d27824cc43e4c5ccd7
mohsensami <mohsensami18727@gmail.com>
2024-11-23 23:08:57 +0330

    Navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import Image from './Image';
+// import Image from './Image';
 import { Link } from 'react-router-dom';
-import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+// import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="w-full h-16 md:h-20 flex items-center justify-between">
             {/* LOGO */}
             <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
-                <Image src="logo.png" alt="Lama Logo" w={32} h={32} />
+                {/* <Image src="logo.png" alt="Lama Logo" w={32} h={32} /> */}
                 <span>lamalog</span>
             </Link>
             {/* MOBILE MENU */}
@@ -66,14 +66,14 @@ const Navbar = () => {
                 <Link to="/posts?sort=trending">Trending</Link>
                 <Link to="/posts?sort=popular">Most Popular</Link>
                 <Link to="/">About</Link>
-                <SignedOut>
+                {/* <SignedOut>
                     <Link to="/login">
                         <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
                     </Link>
-                </SignedOut>
-                <SignedIn>
+                </SignedOut> */}
+                {/* <SignedIn>
                     <UserButton />
-                </SignedIn>
+                </SignedIn> */}
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './routes/Homepage.jsx';
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+    {
+        // element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <App />,
+            },
+        ],
+    },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
