8b02e5bbd0d1687a1198fa95890938861d9d639e
mohsensami <mohsensami18727@gmail.com>
2024-11-26 22:55:28 +0330

    connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+// import userRouter from './routes/user.route.js';
+import connectDB from './lib/connectDB.js';
 
 const app = express();
 
+// app.use('/users', userRouter);
+
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running!');
 });
