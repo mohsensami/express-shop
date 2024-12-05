314466a4a5ccbd98de0177c2240d7f0db5a6b93a
mohsensami <mohsensami18727@gmail.com>
2024-12-05 22:20:28 +0330

    body-parser get input data

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const path = require('path');
-const express = require('express');
 
-const adminRoutes = require('./routes/admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(adminRoutes);
-app.use(shopRoutes);
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res) => {
     res.status(404).send('Page Not Found!');
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    console.log(adminData.products);
     res.sendFile(path.join(__dirname, '../', 'views/html', 'shop.html'));
 });
 
