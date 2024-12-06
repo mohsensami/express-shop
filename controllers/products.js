0c56ee8beec9d46422d1c9c29d34f9c67338ef3b
mohsensami <mohsensami18727@gmail.com>
2024-12-06 17:24:19 +0330

    add models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/single-product');
 
 module.exports.addProductPage = (req, res) => {
     res.render('add-product', {
@@ -7,12 +7,13 @@ module.exports.addProductPage = (req, res) => {
 };
 
 module.exports.sendAllProducts = (req, res) => {
-    products.push({ title: req.body.title });
+    const products = new Product(req.body.title);
+    products.saveProductData();
     res.redirect('/');
 };
 
 module.exports.getAllProducts = (req, res) => {
-    // const products = adminData.products;
+    const products = Product.fetchAllProducts();
     res.render('shop', {
         pageTitle: 'Shop',
         productsArray: products,
---
+++ models/single-product.js
@@ -0,0 +1,13 @@
+const products = [];
+
+module.exports = class Product {
+    constructor(title) {
+        this.title = title;
+    }
+    saveProductData() {
+        products.push(this)
+    }
+    static fetchAllProducts() {
+        return products
+    }
+};
