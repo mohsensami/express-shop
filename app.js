ee786dd731e6afd1d91edc8c6bb05961d8ef6fb6
mohsensami <mohsensami18727@gmail.com>
2024-12-18 21:50:04 +0330

    show image in pages

--- app.js
+++ app.js
@@ -33,9 +33,18 @@ const fileStorage = multer.diskStorage({
     },
 });
 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'))
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
     session({
         secret: 'secret key',
