155cef5145e2bf49736d2e3deede425c0f16b95a
mohsensami <mohsensami18727@gmail.com>
2025-03-04 21:54:22 +0330

    Register page added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,116 +1,116 @@
-const bcrypt = require('bcryptjs');
-const { validationResult } = require('express-validator');
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 module.exports.getLogin = (req, res) => {
-    let message = req.flash('error');
-    if (message.length > 0) {
-        message = message[0];
-    } else {
-        message = null;
-    }
-    res.render('auth/login', {
-        pageTitle: 'Login',
-        isAuth: false,
-        errorMessage: message,
-    });
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/login", {
+    pageTitle: "Login",
+    isAuth: false,
+    errorMessage: message,
+  });
 };
 
 module.exports.postLogin = (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    User.findOne({ email: email })
-        .then((user) => {
-            if (!user) {
-                req.flash('error', 'ایمیل یا رمز عبور اشتباه است');
-                return res.redirect('/login');
-            }
-            bcrypt
-                .compare(password, user.password)
-                .then((result) => {
-                    req.session.isLoggedIn = true;
-                    req.session.user = user;
-                    return req.session.save((err) => {
-                        console.log(err);
-                        res.redirect('/');
-                    });
-                })
-                .catch((err) => {
-                    console.log(err);
-                    req.flash('error', 'ایمیل یا رمز عبور اشتباه است');
-                    res.redirect('/login');
-                });
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "ایمیل یا رمز عبور اشتباه است");
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((result) => {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
         })
         .catch((err) => {
-            console.log(err);
+          console.log(err);
+          req.flash("error", "ایمیل یا رمز عبور اشتباه است");
+          res.redirect("/login");
         });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports.getSingup = (req, res) => {
-    let message = req.flash('error');
-    if (message.length > 0) {
-        message = message[0];
-    } else {
-        message = null;
-    }
-    res.render('auth/signup', {
-        pageTitle: 'signup',
-        isAuth: false,
-        errorMessage: message,
-        lastInput: {
-            email: '',
-            password: '',
-            confirmPassword: '',
-        },
-    });
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/register", {
+    pageTitle: "Register",
+    isAuth: false,
+    errorMessage: message,
+    lastInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
 };
 
 module.exports.postSingup = (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
-    const erorrs = validationResult(req);
-    if (!erorrs.isEmpty()) {
-        console.log(erorrs.array());
-        return res.status(422).render('auth/signup', {
-            pageTitle: 'عضویت',
-            isAuth: false,
-            errorMessage: erorrs.array()[0].msg,
-            lastInput: {
-                email: email,
-                password: password,
-                confirmPassword: confirmPassword,
-            },
-        });
-    }
-    User.findOne({ email: email })
-        .then((userDoc) => {
-            if (userDoc) {
-                req.flash('error', 'ایمیل تکراری است');
-                return res.redirect('/signup');
-            }
-            return bcrypt.hash(password, 12);
-        })
-        .then((hashedPassword) => {
-            const user = new User({
-                email: email,
-                password: hashedPassword,
-                cart: { items: [] },
-            });
-            return user.save();
-        })
-        .then((result) => {
-            res.redirect('/login');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const erorrs = validationResult(req);
+  if (!erorrs.isEmpty()) {
+    console.log(erorrs.array());
+    return res.status(422).render("auth/signup", {
+      pageTitle: "عضویت",
+      isAuth: false,
+      errorMessage: erorrs.array()[0].msg,
+      lastInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
+    });
+  }
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash("error", "ایمیل تکراری است");
+        return res.redirect("/signup");
+      }
+      return bcrypt.hash(password, 12);
+    })
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports.postLogout = (req, res) => {
-    req.session.destroy((err) => {
-        console.log(err);
-        res.redirect('/');
-    });
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,37 @@
-const express = require('express');
-const { check } = require('express-validator');
+const express = require("express");
+const { check } = require("express-validator");
 
-const authControllers = require('../controllers/auth');
+const authControllers = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authControllers.getLogin);
+router.get("/login", authControllers.getLogin);
 
-router.post('/login', authControllers.postLogin);
+router.post("/login", authControllers.postLogin);
 
-router.get('/signup', authControllers.getSingup);
+router.get("/register", authControllers.getSingup);
 
 router.post(
-    '/signup',
-    check('email').isEmail().withMessage('آدرس ایمیل اشتباه است').normalizeEmail(),
-    check('password').isLength({ min: 5 }).withMessage('رمز عبور باید حداقل ۵ کاراکتر باشد').trim(),
-    check('confirmPassword')
-        .trim()
-        .custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('رمز عبور یکسان نیست');
-            }
-            return true;
-        }),
-    authControllers.postSingup
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("آدرس ایمیل اشتباه است")
+    .normalizeEmail(),
+  check("password")
+    .isLength({ min: 5 })
+    .withMessage("رمز عبور باید حداقل ۵ کاراکتر باشد")
+    .trim(),
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("رمز عبور یکسان نیست");
+      }
+      return true;
+    }),
+  authControllers.postSingup
 );
 
-router.post('/logout', authControllers.postLogout);
+router.post("/logout", authControllers.postLogout);
 
 module.exports = router;
